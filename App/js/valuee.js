8bde62e52472fc8dae88bca9537aba6598c3ca57
kamandag88 <43900108+kamandag88@users.noreply.github.com>
2018-11-15 09:26:13 +0800

    Update App/js/valuee.js

--- App/js/valuee.js
+++ App/js/valuee.js
@@ -15,9 +15,24 @@
 
                 coverhead = cpf.get("d2");
                 document.getElementById("cover").innerHTML = coverhead; 
+
+                if(coverhead == 1) {
+                    setbackgcover();
+                }
+                else{
+                    setbackgback();
+                }
             }
 
                 setTimeout("loop()", 1000); 
         }
 
         loop();
+
+        function setbackgcover(){
+            document.body.style.backgroundImage = "url('img/InsertCoverInfo.jpg')";
+        }
+
+        function setbackgback(){
+            document.body.style.backgroundImage = "url('img/6thScreen.jpg')";
+        }
