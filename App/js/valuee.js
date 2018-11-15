899b3fa77594bb346c65c33ea9782f2a4c59a5ad
kamandag88 <43900108+kamandag88@users.noreply.github.com>
2018-11-15 08:56:46 +0800

    Update App/js/valuee.js

--- App/js/valuee.js
+++ App/js/valuee.js
@@ -14,7 +14,7 @@
             if(cpf){
 
                 coverhead = cpf.get("d2");
-                document.getElementById("cover").style.backgroundImage = "url('img/CoverHead.jpg')"; 
+                document.getElementById("cover").innerHTML = coverhead; 
             }
 
                 setTimeout("loop()", 1000); 
