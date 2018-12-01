7551aed4f6ea0439be98fd7e80363ef96a6a3cbb
kamandag88 <43900108+kamandag88@users.noreply.github.com>
2018-12-01 11:09:11 +0800

    Update valuee.js

--- Teacher/js/valuee.js
+++ Teacher/js/valuee.js
@@ -9,9 +9,12 @@
         storeId2 = "store" + userInput;
         alert(storeId2);
         dwdstr = aop.getStore(storeId2);
-        alert(dwdstr);  
+        alert(dwdstr);
+        alert("Tapos1");
         valdwd = JSON.parse(dwdstr);
+        alert("Tapos2");
         alert(valdwd);
+        alert("Tapos3");
         document.getElementById("namemo").innerHTML = valdwd.studentname;
         document.getElementById("classnumber").innerHTML = valdwd.classnumber;
         document.getElementById("gamescore").innerHTML = valdwd.gamescore;
