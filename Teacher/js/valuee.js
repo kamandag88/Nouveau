f32a4b20db23cded58a4f8bb2f93b7e2a8f73f56
kamandag88 <43900108+kamandag88@users.noreply.github.com>
2018-12-02 00:26:54 +0800

    Update valuee.js

--- Teacher/js/valuee.js
+++ Teacher/js/valuee.js
@@ -2,19 +2,14 @@
     var storeId2 = "store" + userInput;
     var dwdstr = "";
     var valdwd = 0;
+    var gscore = 0;
+    var qscore = 0;
 
     function downloadd(){
         userInput= document.getElementById("userInput").value
-        alert(userInput);
         storeId2 = "store" + userInput;
-        alert(storeId2);
         dwdstr = aop.getStore(storeId2);
-        alert(dwdstr);
-        alert("Tapos1");
         valdwd = JSON.parse(dwdstr);
-        alert("Tapos2");
-        alert(valdwd);
-        alert("Tapos3");
         document.getElementById("namemo").innerHTML = valdwd.studentname;
         document.getElementById("classnumber").innerHTML = valdwd.classnumber;
         document.getElementById("gamescore").innerHTML = valdwd.gamescore;
@@ -22,4 +17,11 @@
         document.getElementById("statsminutes").innerHTML = valdwd.timeminutes;
         document.getElementById("statsseconds").innerHTML = valdwd.timeseconds;
         document.getElementById("statstens").innerHTML = valdwd.timetens;
+
+        gscore = valdwd.gamescore.toString();
+        qscore = valdwd.questscore.toString();
+
+        if(gscore >= 500 &&){
+
+        }
         }
