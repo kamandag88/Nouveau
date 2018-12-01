c5745583cdf55ea0324ec7bdba3c27da7346e02d
kamandag88 <43900108+kamandag88@users.noreply.github.com>
2018-12-01 10:49:28 +0800

    Create valuee.js

---
+++ Teacher/js/valuee.js
@@ -0,0 +1,24 @@
+    var userInput = 0;
+    var storeId2 = "store" + userInput;
+    var dwdstr = "";
+    var valdwd = 0;
+
+    function downloadd(){
+        userInput= document.getElementById("userInput").value
+        alert(userInput);
+        storeId2 = "store" + userInput;
+        alert(storeId2);
+        dwdstr = aop.getStore(storeId2);
+        alert(dwdstr);
+        document.getElementById("DWDstr").innerHTML = dwdstr;    
+        valdwd = JSON.parse(dwdstr);
+        alert(valdwd);
+        document.getElementById("namemo").innerHTML = valdwd.nameinput;
+        document.getElementById("classnumber").innerHTML = valdwd.classnumber;
+        document.getElementById("gamescore").innerHTML = valdwd.gamescore;
+        document.getElementById("questscore").innerHTML = valdwd.questscore;
+        document.getElementById("statsminutes").innerHTML = valdwd.timeminutes;
+        document.getElementById("statsseconds").innerHTML = valdwd.timeseconds;
+        document.getElementById("statstens").innerHTML = valdwd.timetens;
+
+        }
