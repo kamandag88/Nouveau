d4774ae52af318952e01fa38c24627e652006533
kamandag88 <43900108+kamandag88@users.noreply.github.com>
2018-12-01 11:04:45 +0800

    Update valuee.js

--- Teacher/js/valuee.js
+++ Teacher/js/valuee.js
@@ -12,7 +12,7 @@
         alert(dwdstr);  
         valdwd = JSON.parse(dwdstr);
         alert(valdwd);
-        document.getElementById("namemo").innerHTML = valdwd.nameinput;
+        document.getElementById("namemo").innerHTML = valdwd.studentname;
         document.getElementById("classnumber").innerHTML = valdwd.classnumber;
         document.getElementById("gamescore").innerHTML = valdwd.gamescore;
         document.getElementById("questscore").innerHTML = valdwd.questscore;
